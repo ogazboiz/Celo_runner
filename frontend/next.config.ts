0968e4f24ba32e1517f8cb574d208160607862b0
ogazboiz <ogazboizakpolo@gmail.com>
2025-11-26 23:07:21 +0100

    Upgrade to Next.js 15.2.1 and match RhythmRush setup to fix Vercel 404 errors

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,42 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Add empty turbopack config to silence warning when using webpack
-  turbopack: {},
-  
-  webpack: (config, { isServer, webpack }) => {
-    // Use IgnorePlugin to exclude test files and other unnecessary files
-    config.plugins = config.plugins || [];
-    config.plugins.push(
-      new webpack.IgnorePlugin({
-        resourceRegExp: /^\.\/test/,
-        contextRegExp: /thread-stream/,
-      }),
-      new webpack.IgnorePlugin({
-        resourceRegExp: /^\.\/bench\.js$/,
-        contextRegExp: /thread-stream/,
-      }),
-      new webpack.IgnorePlugin({
-        resourceRegExp: /^\.\/README\.md$/,
-        contextRegExp: /thread-stream/,
-      }),
-      new webpack.IgnorePlugin({
-        resourceRegExp: /^\.\/LICENSE$/,
-        contextRegExp: /thread-stream/,
-      })
-    );
-
-    // Ignore warnings for these files
-    config.ignoreWarnings = [
-      { module: /node_modules\/thread-stream/ },
-      { file: /node_modules\/thread-stream\/test/ },
-      { file: /node_modules\/thread-stream\/bench\.js/ },
-      { file: /node_modules\/thread-stream\/README\.md/ },
-      { file: /node_modules\/thread-stream\/LICENSE/ },
-    ];
-
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
